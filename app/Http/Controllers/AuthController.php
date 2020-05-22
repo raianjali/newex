03f7a4838e84a892123e065ebaec65d66359b3bf
Anjali Rai <raianjali280397@gmail.com>
2020-05-22 18:31:31 +0530

    Update AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,52 +2,95 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+/*use Illuminate\Http\Request;
+//use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Illuminate\Support\Facades\Validator;
 use App\Http\Traits\JsonUtilTrait;
 use App\User;
-use App\Roles;
 use Throwable;
+use Log;*/
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+
+use App\User;
+use Illuminate\Support\Facades\Request;
 use Log;
+use Hash;
+use App\Http\Requests;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    use JsonUtilTrait;
+    
     /**
      * Create a new AuthController instance.
      *
      * @return void
      */
     public function login(Request $request){
-        try {
-                $validator = Validator::make($request->all(), [
-                    "role" => "required|integer|exists:roles,role_id",
-                    "email" => "string",
-                    "password" => "required",
-                ]);
-
-                if ($validator->fails()) {
-                    return $this->responseWithError("Validation Error", $validator->messages(), 400);
-                }
                 
-                $role = $request->role;
-                if($role==1 || $role==2 || $role==3){
-                    $credentials = $request->only(['email', 'password']);
+                if (request::has(['email','password', 'role_id']))
+                {
+                    $credentials = request(['email', 'password', 'role_id']);
+                    try
+                    {
+                        if ($token = auth()->attempt($credentials)) 
+                        {
+                            $user = User::where('email', '=', $credentials['email'])->first();
+                            if($user->status==1&&$user->role_id==$credentials['role_id'])
+                            {
+                                return response()->json(['Results' =>['token'=>$token,'user'=>$user],'message'=>'Succesful','requestLocation'=> Request::getRequestUri(),'status'=> 200,'success'=> true,'metadata'=>(object) array()],200);
+                            }
+                            else
+                            {
+                                return response()->json(['Results' =>(object) array(),'message'=>'Unauthorized','requestLocation'=> Request::getRequestUri(),'status'=> 401,'success'=> false,'metadata'=>(object) array()],200);
+                            } 
+                        }
+                        else
+                        {
+                            return response()->json(['Results' =>(object) array(),'message'=>'Unauthorized','requestLocation'=> Request::getRequestUri(),'status'=> 401,'success'=> false,'metadata'=>(object) array()],200);
+                        }
+                    }
+                    catch(JWTException $je)
+                    {
+                        return response()->json(['Results' =>(object) array(),'message'=>'could_not_create_token','requestLocation'=> Request::getRequestUri(),'status'=> 500,'success'=> false,'metadata'=>(object) array()],500);
+                    }
                 }
-                if($role==4){
-                    $credentials = $request->only(['roll_no', 'password']);
-                }
-                if (!$token = JWTAuth::attempt($credentials)) {
-                    return 'Invalid login details';
-                }
+                else if (request::has(['roll_no','password', 'role_id']))
+                {
+                    $credentials = request(['roll_no', 'password', 'role_id']);
 
-                return $token;
-        } 
-        catch (Throwable $e) {
-            Log::error($e->getMessage());
-            return $this->responseWithError('Internal server error', [], 500, 500);
-        } 
-        
-     }
+                    try
+                    {
+                        if ($token = auth()->attempt($credentials)) 
+                        {
+                            $user = User::where('roll_no', '=', $credentials['roll_no'])->first();
+                            if($user->status==1&&$user->role_id==$credentials['role_id'])
+                            {
+                                return response()->json(['Results' =>['token'=>$token,'user'=>$user],'message'=>'Succesful','requestLocation'=> Request::getRequestUri(),'status'=> 200,'success'=> true,'metadata'=>(object) array()],200);
+                            }
+                            else
+                            {
+                                return response()->json(['Results' =>(object) array(),'message'=>'Unauthorized','requestLocation'=> Request::getRequestUri(),'status'=> 401,'success'=> false,'metadata'=>(object) array()],200);
+                            } 
+                        }
+                        else
+                        {
+                            return response()->json(['Results' =>(object) array(),'message'=>'Unauthorized','requestLocation'=> Request::getRequestUri(),'status'=> 401,'success'=> false,'metadata'=>(object) array()],200);
+                        }
+                    }
+                    catch(JWTException $je)
+                    {
+                        return response()->json(['Results' =>(object) array(),'message'=>'could_not_create_token','requestLocation'=> Request::getRequestUri(),'status'=> 500,'success'=> false,'metadata'=>(object) array()],500);
+                    }
+                }
+                else
+                {
+                    return response()->json(['Results' =>(object) array(),'message'=>'Bad request','requestLocation'=> Request::getRequestUri(),'status'=> 400,'success'=> false,'metadata'=>(object) array()],200);
+                }
+   
+    }
 }
